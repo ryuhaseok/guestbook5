08155ad12b941325a60c6443916a5f264147993d
rhs0402 <rhs0402@naver.com>
2024-02-28 18:06:41 +0900

    2024-02-28 mybatis

--- src/main/java/com/javaex/dao/GuestDao.java
+++ src/main/java/com/javaex/dao/GuestDao.java
@@ -1,13 +1,9 @@
 package com.javaex.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.javaex.vo.GuestVo;
@@ -15,158 +11,39 @@ import com.javaex.vo.GuestVo;
 @Repository
 public class GuestDao {
 	
-	// 필드
-		private Connection conn = null;
-		private PreparedStatement pstmt = null;
-		private ResultSet rs = null;
-		private String driver = "com.mysql.cj.jdbc.Driver";
-		private String url = "jdbc:mysql://localhost:3306/web_db";
-		private String id = "web";
-		private String pw = "web";
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	//삭제
+	public int guestDelete(GuestVo guestVo) {
+		System.out.println("GeustDao.guestDelete()");
 		
-		// 연결
-		public void getConnection() {
-			try {
-				// 1. JDBC 드라이버 (Oracle) 로딩
-				Class.forName(driver);
-
-				// 2. Connection 얻어오기
-				conn = DriverManager.getConnection(url, id, pw);
-
-			} catch (ClassNotFoundException e) {
-				System.out.println("error: 드라이버 로딩 실패 - " + e);
-
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-
-			}
-		}
-
-		// 종료
-		public void close() {
-			// 5. 자원정리
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				System.out.println("error:" + e);
-			}
-		}
-
+		int count = sqlSession.delete("guestbook.delete", guestVo);
+		
+		return count;
+	}
+	
+	
+	//등록
 	public int insertGuestbook(GuestVo guestVo) {
+		System.out.println("GeustDao.insertGuestbook()");
 		
-		int count = -1;
-		
-		this.getConnection();
-		
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = " insert into guestbook "
-					+ " values(null, ?, ?, ?, now()) ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setString(1, guestVo.getName());
-			pstmt.setString(2, guestVo.getPw());
-			pstmt.setString(3, guestVo.getContent());
-
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			System.out.println(count + "건 등록되었습니다.");
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		
-		this.close();
+		int count = sqlSession.insert("guestbook.insert", guestVo);
 		
 		return count;
 	}//
 	
+	
+	//전체 가져오기
 	public List<GuestVo> guestSelect() {
+		System.out.println("GeustDao.guestSelect()");
 		
-		List<GuestVo> guestList = new ArrayList<GuestVo>();
-		
-		this.getConnection();
-		
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = " select  no,"
-						+ "		     name,"
-						+ "          reg_date,"
-						+ "          content"
-						+ " from guestbook ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			
-			rs = pstmt.executeQuery();
-
-			// 4.결과처리
-			
-			while(rs.next()) {
-				int no = rs.getInt("no");
-				String name = rs.getString("name");
-				String rd = rs.getString("reg_date");
-				String content = rs.getString("content");
-				
-				GuestVo guestVo = new GuestVo(no, name, rd, content);
-				
-				guestList.add(guestVo);
-				
-			}
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		
-		this.close();
+		List<GuestVo> guestList = sqlSession.selectList("guestbook.select");
+		System.out.println(guestList);
 		
 		return guestList;
 	}//
 	
-	public int guestDelete(GuestVo guestVo) {
-		
-		int count = -1;
-		
-		this.getConnection();
-		
-		try {
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// SQL문 준비
-			String query = " delete from guestbook "
-							+ "where no = ? "
-							+ "and password = ? ";
-
-			// 바인딩
-			pstmt = conn.prepareStatement(query);
-			pstmt.setInt(1, guestVo.getNo());
-			pstmt.setString(2, guestVo.getPw());
-
-			// 실행
-			count = pstmt.executeUpdate();
-
-			// 4.결과처리
-			System.out.println(count + "건 삭제되었습니다.");
-
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} 
-		
-		this.close();
-		
-		return count;
-	}
 	
 }
